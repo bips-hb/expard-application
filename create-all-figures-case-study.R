9405fa495526208099f5311943058cc3310d49d9
Louis Dijkstra <louisdijkstra@gmail.com>
2023-11-29 23:43:29 +0100

    Added scripts for creating all figures EHC case study

---
+++ create-all-figures-case-study.R
@@ -0,0 +1,107 @@
+library(expard)
+library(readr)
+library(ggplot2)
+
+get_fit <- function(i) {
+  readr::read_rds(fit_filenames[i])
+}
+
+
+plot_fit_past <- function(i, add_current_use = TRUE,
+                          x_label = "past parameter (in quarter years)", 
+                          y_label = "BIC",
+                          title = "",
+                          y_range = NULL, 
+                          past_range = c(1,55)) {
+  
+  fit <- get_fit(i)
+  
+  fit <- fit %>% filter(model == 'past-use' | model == 'current-use') %>% arrange(model)
+  fit$model = c("current use", rep('past use', 55))
+  
+  if (add_current_use) {
+    fit <- fit %>% filter(model == 'past use' | model == 'current use') %>%
+      arrange(model)
+    fit[1, 'past'] <- 0 
+    
+    fit <- fit %>% 
+      filter((past >= past_range[1] & past <= past_range[2]) | past == 0)
+    
+  } else {
+    fit <- fit %>% filter(model == 'past use') %>% 
+      filter(past >= past_range[1] & past <= past_range[2]) 
+  }
+  
+  ggplot(fit, mapping = aes(x = past, y = BIC, color = model)) + 
+    geom_point() + 
+    scale_color_manual(values = c("current use" = "#fa8537", "past use" = "#1763aa")) + 
+    coord_cartesian(ylim=y_range) + 
+    ggtitle(title) + 
+    theme_bw() +
+    xlab(x_label) +
+    ylab(y_label) + 
+    scale_x_continuous(breaks = seq(0, 56, by = 20),
+                       minor_breaks = seq(0, 56, by = 4))
+}
+
+fit_filenames <-
+  c(
+    "results/fit_penicillin_shock.rds", #
+    "results/fit_doacs_bleeding.rds",   # 
+    "results/fit_nsaids_hf.rds",        # 
+    "results/fit_oc_shock.rds",         
+    "results/fit_antibiotics_bleeding.rds",  # negative control
+    "results/fit_atd_bleeding.rds",
+    "results/fit_diabetics_bleeding.rds",    # negative control
+    "results/fit_psychotics_type2diabetes.rds", #
+    "results/fit_psychotics_clo_type2diabetes.rds",
+    "results/fit_psychotics_ola_type2diabetes.rds",
+    "results/fit_psychotics_que_type2diabetes.rds"
+  )
+
+# "results/fit_penicillin_shock.rds"
+i = 1
+
+fit <- get_fit(i) %>% filter(model == 'no-association')
+fit$BIC
+p <- expard::plot_fit(fit = get_fit(i), 
+                      title = "penicillin and anaphylaxis", 
+                      y_range = c(5150, 5230))
+ggsave("results/figure_penicillin_shock.pdf", p, width = 5, height = 5)
+
+# "results/fit_doacs_bleeding.rds"
+i = 2
+
+fit <- get_fit(i) %>% filter(model == 'no-association')
+fit$BIC
+
+p <- expard::plot_fit(fit = get_fit(i), 
+                      title = "DOACs and bleeding", 
+                      y_range = c(224500, 226000))
+ggsave("results/figure_doacs_bleeding.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "DOACs and bleeding")
+ggsave("results/past_figure_doacs_bleeding.pdf", p, width = 4.5, height = 4)
+
+# "results/fit_antibiotics_bleeding.rds"
+i = 5
+
+p <- expard::plot_fit(fit = get_fit(i), 
+                      title = "antibiotics and bleeding")
+ggsave("results/figure_antibiotics_bleeding.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "antibiotics and bleeding")
+ggsave("results/past_figure_antibiotics_bleeding.pdf", p, width = 4.5, height = 4)
+
+# "results/fit_psychotics_type2diabetes.rds"
+i = 8
+
+fit <- get_fit(i) %>% filter(model == 'no-association')
+fit$BIC
+
+p <- expard::plot_fit(get_fit(i), y_range = c(214000, 216300),
+                      title = "antipsychotics and type 2 diabetes")
+ggsave("results/figure_psychotics_type2diabetes.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "antipsychotics and type 2 diabetes")
+ggsave("results/past_figure_psychotics_type2diabetes.pdf", p, width = 4.5, height = 4)
--- exploring-results.R
+++ exploring-results.R
@@ -95,7 +95,7 @@ plot_fit_with_breakline <- function(i, x_label = "model",
 }
 
 plot_fit_past <- function(i, add_current_use = TRUE,
-                          x_label = "past parameter", 
+                          x_label = "past parameter (in quarter years)", 
                           y_label = "BIC",
                           title = "",
                           y_range = NULL, 
@@ -126,38 +126,126 @@ plot_fit_past <- function(i, add_current_use = TRUE,
     ggtitle(title) + 
     theme_bw() +
     xlab(x_label) +
-    ylab(y_label)
+    ylab(y_label) + 
+    scale_x_continuous(breaks = seq(0, 56, by = 20),
+                       minor_breaks = seq(0, 56, by = 4))
 }
 
 
 fit_filenames <-
   c(
-    "results/fit_penicillin_shock.rds", 
-    "results/fit_doacs_bleeding.rds", 
-    "results/fit_nsaids_hf.rds", 
-    "results/fit_oc_shock.rds", 
-    "results/fit_antibiotics_bleeding.rds",  
+    "results/fit_penicillin_shock.rds", #
+    "results/fit_doacs_bleeding.rds",   # 
+    "results/fit_nsaids_hf.rds",        # 
+    "results/fit_oc_shock.rds",         
+    "results/fit_antibiotics_bleeding.rds",  # negative control
     "results/fit_atd_bleeding.rds",
-    "results/fit_diabetics_bleeding.rds",
-    "results/fit_psychotics_type2diabetes.rds",
+    "results/fit_diabetics_bleeding.rds",    # negative control
+    "results/fit_psychotics_type2diabetes.rds", #
     "results/fit_psychotics_clo_type2diabetes.rds",
     "results/fit_psychotics_ola_type2diabetes.rds",
     "results/fit_psychotics_que_type2diabetes.rds"
   )
 
-i <- 6
+i <- 8
 
 plot_fit_local(i, exclude_null = FALSE)
 plot_fit_local(i, exclude_null = TRUE)
-plot_fit_past(i, add_current_use = T)#, past_range = c(0,20)) #, past_range = c(1,10))
+plot_fit_past(i, add_current_use = T, past_range = c(0,20)) #, past_range = c(1,10))
+
+
+# we use: "results/fit_penicillin_shock.rds"
+i = 1
+plot_fit_local(i, exclude_null = FALSE)
+plot_fit_local(i, exclude_null = TRUE)
+# plot_fit_past(i, add_current_use = T)
+
+fit <- get_fit(i)
+r = fit %>% filter(model == 'no-association')
+r$BIC
+p <- expard::plot_fit(fit = get_fit(i), 
+                 title = "penicillin and anaphylaxis", 
+                 y_range = c(5150, 5230)
+)
+
+ggsave("results/figure_penicillin_shock.pdf", p, width = 5, height = 5)
+
+# "results/fit_doacs_bleeding.rds"
+i = 2
+plot_fit_local(i, exclude_null = FALSE)
+plot_fit_local(i, exclude_null = TRUE)
+plot_fit_past(i, add_current_use = T, title = "DOACs and bleeding")
+
+
+fit <- get_fit(i)
+min(fit$BIC)
+r = fit %>% filter(model == 'no-association')
+r$BIC
+p <- expard::plot_fit(fit = get_fit(i), 
+                      title = "DOACs and bleeding", 
+                      y_range = c(224500, 226000)
+)
+
+ggsave("results/figure_doacs_bleeding.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "DOACs and bleeding")
+
+ggsave("results/past_figure_doacs_bleeding.pdf", p, width = 4.5, height = 4)
+
+
+# "results/fit_antibiotics_bleeding.rds"
+i = 5
+plot_fit_local(i, exclude_null = FALSE) # note that no association is more likely
+# plot_fit_local(i, exclude_null = TRUE)
+plot_fit_past(i, add_current_use = T) # long term effect? just time
+
+p <- expard::plot_fit(fit = get_fit(i), 
+                      title = "antibiotics and bleeding"
+)
+
+ggsave("results/figure_antibiotics_bleeding.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "antibiotics and bleeding")
+
+ggsave("results/past_figure_antibiotics_bleeding.pdf", p, width = 4.5, height = 4)
+
+
+# "results/fit_psychotics_type2diabetes.rds"
+i = 8
+plot_fit_local(i, exclude_null = FALSE) # note that no association is more likely
+plot_fit_local(i, exclude_null = TRUE)
+
+
+fit <- get_fit(i)
+r = fit %>% filter(model == 'no-association')
+r$BIC
+
+p <- expard::plot_fit(get_fit(i), y_range = c(214000, 216300),
+                      title = "antipsychotics and type 2 diabetes")
+ggsave("results/figure_psychotics_type2diabetes.pdf", p, width = 5, height = 5)
+
+p <- plot_fit_past(i, add_current_use = T, title = "antipsychotics and type 2 diabetes")
+ggsave("results/past_figure_psychotics_type2diabetes.pdf", p, width = 4.5, height = 4)
+
+###############################################
+
+# "results/fit_nsaids_hf.rds"
+i = 3
+plot_fit_local(i, exclude_null = FALSE) # note that no association is more likely
+#plot_fit_local(i, exclude_null = TRUE)
+plot_fit_past(i, add_current_use = T) # long term effect? just time
+
+
+# "results/fit_diabetics_bleeding.rds" 
+i = 7
+plot_fit_local(i, exclude_null = FALSE) # note that no association is more likely
+plot_fit_local(i, exclude_null = TRUE)
+plot_fit_past(i, add_current_use = T) # long term effect? just time
 
 
 
-
-
-
-# expard::plot_risk(drug_history = c(rep(0,4), rep(1,5), rep(0, 47)), 
-#                  risk_model = expard::risk_model_past(30))
+expard::plot_risk(drug_history = c(rep(0,4), rep(1,20), rep(0, 27)), 
+                  risk_model = expard::risk_model_past(4))
 
 
 fit_filename = fit_filenames[4]
